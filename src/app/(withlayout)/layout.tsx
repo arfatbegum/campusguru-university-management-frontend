3a3413213f329fad02a693e42b101b8e2720cb1b
arfat132 <arfatbegum90@gmail.com>
2023-09-29 20:16:46 +0300

    manage private route

--- src/app/(withlayout)/layout.tsx
+++ src/app/(withlayout)/layout.tsx
@@ -1,8 +1,34 @@
+"use client";
 import Contents from "@/components/Ui/Contents";
 import SideBar from "@/components/Ui/Sidebar";
-import { Layout } from "antd";
+import { isLoggedIn } from "@/services/auth.service";
+import { Layout, Spin } from "antd";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const userLoggedIn = isLoggedIn();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!userLoggedIn) {
+      router.push("/login");
+    }
+    setIsLoading(true);
+  }, [userLoggedIn, router, isLoading]);
+
+  if (!isLoading) {
+    return <Spin tip="Loading" size="large"
+      style={{
+        minHeight: "100vh",
+        color:"#4338ca"
+      }}
+    >
+      <div className="content" />
+    </Spin>;
+  }
+
   return (
     <Layout hasSider>
       <SideBar />
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,11 @@ import { Spin } from 'antd';
 const Loading = () => {
     return (
         <div>
-            <Spin tip="Loading" size="large">
+            <Spin tip="Loading" size="large"
+                style={{
+                    minHeight: "100vh",
+                    color:"#4338ca"
+                }}>
                 <div className="content" />
             </Spin>
         </div>
