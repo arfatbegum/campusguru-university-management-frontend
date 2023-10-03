bd0c6de61f5b9ae5f72fd48bfe4f4d61e338ef6b
arfat132 <arfatbegum90@gmail.com>
2023-10-03 04:15:47 +0300

    [done] department create funtionality

--- src/app/(withlayout)/super_admin/department/create/page.tsx
+++ src/app/(withlayout)/super_admin/department/create/page.tsx
@@ -3,19 +3,24 @@
 import UMBreadCrumb from '@/components/Ui/UMBreadCrumb';
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
-import { Col, Row } from "antd";
+import { Col, Row, message } from "antd";
 import { departmentSchema } from '@/schemas/department';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useAddDepartmentMutation } from '@/redux/api/departmentApi';
 
 const CreateDepartment = () => {
+    const [addDepartment] = useAddDepartmentMutation();
+
     const onSubmit = async (data: any) => {
+        message.loading("Creating.....");
         try {
-            console.log(data);
+            await addDepartment(data);
+            message.success("Department added successfully");
         } catch (err: any) {
-            console.error(err.message);
+            message.error(err.message);
         }
     };
-
+    
     return (
         <div>
             <UMBreadCrumb
@@ -34,7 +39,7 @@ const CreateDepartment = () => {
 
             <Form submitHandler={onSubmit} resolver={yupResolver(departmentSchema)}>
                 <div className="lg:w-1/2 bg-white border border-gray-200 p-5 pb-6 rounded mb-4 shadow-sm" >
-                <p className="font-semibold text-lg mb-3">
+                    <p className="font-semibold text-lg mb-3">
                         Department information
                     </p>
                     <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }} >
@@ -43,7 +48,7 @@ const CreateDepartment = () => {
                         </Col>
                     </Row>
                 </div>
-                <button className="bg-indigo-700 px-4 py-2 text-white rounded font-semibold">Create Department</button>
+                <button type="submit" className="bg-indigo-700 px-4 py-2 text-white rounded font-semibold">Create Department</button>
             </Form>
         </div>
     );
