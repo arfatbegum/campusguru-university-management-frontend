d822c6b35913bcfab76c0c45c19b105a310b192c
arfat132 <arfatbegum90@gmail.com>
2023-10-04 19:03:51 +0300

    Create admin api

---
+++ src/redux/api/adminApi.ts
@@ -0,0 +1,59 @@
+import { IAdmin, IMeta } from "@/types";
+import { baseApi } from "./baseApi";
+import { tagTypes } from "../tagTypes";
+
+const ADMIN_URL = "/admins";
+
+export const adminApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    addAdminWithFormData: build.mutation({
+      query: (data) => ({
+        url: "/users/create-admin",
+        method: "POST",
+        data,
+        contentType: "multipart/form-data",
+      }),
+      invalidatesTags: [tagTypes.admin],
+    }),
+    admins: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: ADMIN_URL,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformResponse: (response: IAdmin[], meta: IMeta) => {
+        return {
+          admins: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.admin],
+    }),
+    admin: build.query({
+        query: (id) => ({
+          url: `${ADMIN_URL}/${id}`,
+          method: "GET",
+        }),
+        providesTags: [tagTypes.department],
+    }),
+    updateAdmin: build.mutation({
+        query: (data) => ({
+          url: `${ADMIN_URL}/${data.id}`,
+          method: "PATCH",
+          data: data.body,
+        }),
+        invalidatesTags: [tagTypes.department],
+    }),
+    deleteAdmin: build.mutation({
+        query: (id) => ({
+          url: `${ADMIN_URL}/${id}`,
+          method: "DELETE",
+        }),
+        invalidatesTags: [tagTypes.department],
+      }),
+  }),
+});
+
+export const { useAdminsQuery, useAddAdminWithFormDataMutation } = adminApi;
