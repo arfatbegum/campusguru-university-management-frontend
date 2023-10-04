e2028a71a9a8ea29530f1ad4fafc0c5d241606b6
arfat132 <arfatbegum90@gmail.com>
2023-10-04 20:28:18 +0300

    [done] get all admins and search functionality

--- src/app/(withlayout)/super_admin/admin/page.tsx
+++ src/app/(withlayout)/super_admin/admin/page.tsx
@@ -1,29 +1,172 @@
+"use client";
 import ActionBar from "@/components/Ui/ActionBar";
 import UMBreadCrumb from "@/components/Ui/UMBreadCrumb";
+import { Button, Input } from "antd";
 import Link from "next/link";
+import {
+    DeleteOutlined,
+    EditOutlined,
+    SearchOutlined,
+    ReloadOutlined,
+    EyeOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+import { useDebounced } from "@/redux/hooks";
+import UMTable from "@/components/Ui/UMTable";
+import { useAdminsQuery } from "@/redux/api/adminApi";
+import { IDepartment } from "@/types";
+import dayjs from "dayjs";
 
-const Admin = () => {
+const AdminPage = () => {
+    const query: Record<string, any> = {};
+
+    const [page, setPage] = useState<number>(1);
+    const [size, setSize] = useState<number>(10);
+    const [sortBy, setSortBy] = useState<string>("");
+    const [sortOrder, setSortOrder] = useState<string>("");
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    query["limit"] = size;
+    query["page"] = page;
+    query["sortBy"] = sortBy;
+    query["sortOrder"] = sortOrder;
+
+    const debouncedSearchTerm = useDebounced({
+        searchQuery: searchTerm,
+        delay: 600,
+    });
+
+    if (!!debouncedSearchTerm) {
+        query["searchTerm"] = debouncedSearchTerm;
+    }
+    const { data, isLoading } = useAdminsQuery({ ...query });
+    const admins = data?.admins;
+    const meta = data?.meta;
+
+    const columns = [
+        {
+            title: "Id",
+            dataIndex: "id",
+            sorter: true,
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+            render: function (data: Record<string, string>) {
+                const fullName = `${data?.firstName} ${data?.middleName} ${data?.lastName}`;
+                return <>{fullName}</>;
+            },
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+        },
+        {
+            title: "Department",
+            dataIndex: "managementDepartment",
+            render: function (data: IDepartment) {
+                return <>{data?.title}</>;
+            },
+        },
+        {
+            title: "Designation",
+            dataIndex: "designation",
+        },
+        {
+            title: "Created at",
+            dataIndex: "createdAt",
+            render: function (data: any) {
+                return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+            },
+            sorter: true,
+        },
+        {
+            title: "Contact no.",
+            dataIndex: "contactNo",
+        },
+        {
+            title: "Action",
+            dataIndex: "id",
+            render: function (data: any) {
+                return (
+                    <>
+                        <Link href={`/super_admin/admin/details/${data.id}`}>
+                            <button className="bg-indigo-700 text-white font-bold py-1 px-2 rounded mr-2">
+                                <EyeOutlined />
+                            </button>
+                        </Link>
+                        <Link href={`/super_admin/admin/update/${data.id}`}>
+                            <button className="bg-indigo-700 text-white font-bold py-1 px-2 rounded mr-2">
+                                <EditOutlined />
+                            </button>
+                        </Link>
+                        <button className="bg-red-500 text-white font-bold py-1 px-2 rounded mr-2">
+                            <DeleteOutlined />
+                        </button>
+                    </>
+                );
+            },
+        },
+    ];
+    const onPaginationChange = (page: number, pageSize: number) => {
+        console.log("Page:", page, "PageSize:", pageSize);
+        setPage(page);
+        setSize(pageSize);
+    };
+    const onTableChange = (pagination: any, filter: any, sorter: any) => {
+        const { order, field } = sorter;
+        setSortBy(field as string);
+        setSortOrder(order === "ascend" ? "asc" : "desc");
+    };
+
+    const resetFilters = () => {
+        setSortBy("");
+        setSortOrder("");
+        setSearchTerm("");
+    };
     return (
         <div>
             <UMBreadCrumb
                 items={[
                     {
-                        label: "Super Admin",
+                        label: "super_admin",
                         link: "/super_admin",
-                    },
-                    {
-                        label: "Admin",
-                        link: "/admin",
                     },
                 ]}
             />
             <ActionBar title="Admin List">
-                <Link href="/super_admin/admin/create">
-                    <button className="bg-indigo-700 px-4 py-2 text-white rounded font-semibold float-right">Create Admin</button>
+                <Input
+                    addonBefore={<SearchOutlined style={{ fontSize: '18px', color: "#4338ca" }} />}
+                    placeholder="Search admin ......"
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+                    }}
+                />
+                {(!!sortBy || !!sortOrder || !!searchTerm) && (
+                    <button
+                        onClick={resetFilters}
+                        className="bg-indigo-700 px-4 py-2 ml-2 text-white rounded font-semibold float-right"
+                    >
+                        <ReloadOutlined />
+                    </button>
+                )}
+                <Link href="/super_admin/department/create">
+                    <button className="bg-indigo-700 px-4 py-2 ml-2 text-white rounded font-semibold float-right">Create</button>
                 </Link>
             </ActionBar>
+            <UMTable
+                loading={isLoading}
+                columns={columns}
+                dataSource={admins}
+                pageSize={size}
+                totalPages={meta?.total}
+                showSizeChanger={true}
+                onPaginationChange={onPaginationChange}
+                onTableChange={onTableChange}
+                showPagination={true}
+            />
         </div>
     );
 };
 
-export default Admin;
+export default AdminPage;
